18148dfd896ed66f2b3258e39645c2db5a6af7d3
unknown <gregory.wow@hotmail.com>
2024-06-21 00:36:13 -0300

    tests

--- Giraffe.Poc.Tests/Tests.fs
+++ Giraffe.Poc.Tests/Tests.fs
@@ -1,8 +1,22 @@
 module Tests
 
+open FluentAssertions
 open System
 open Xunit
+
+let sum a b = a + b
 
 [<Fact>]
 let ``My test`` () =
     Assert.True(true)
+    
+[<Fact>]
+let ``Test test`` () =
+    let result = (sum 10 10)
+    result.Should().Be(20, "")
+
+[<Fact>]
+let ``Remove product`` () =
+   let result = (ProductHandler.remove 1)
+   result.Should().BeNull("")
+   
