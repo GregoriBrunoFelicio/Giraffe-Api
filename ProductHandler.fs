880e004860e753450391b508149e2dd9e72c7505
unknown <gregory.wow@hotmail.com>
2024-06-23 23:21:41 -0300

    add comments

--- ProductHandler.fs
+++ ProductHandler.fs
@@ -7,15 +7,21 @@ open System.Linq
 open Models
 
 let getAllProducts: HttpHandler =
-    fun (next: HttpFunc) (ctx: HttpContext) -> task {
-        let products = ctx.GetService<GiraffeContext>().products.ToList<Product>() |> List.ofSeq
-        return! json products next ctx
-    }
+    fun (next: HttpFunc) (ctx: HttpContext) ->
+        task {
+            let products =
+                ctx.GetService<GiraffeContext>().products.ToList<Product>() |> List.ofSeq
+
+            return! json products next ctx
+        }
 
 let getById id : HttpHandler =
     fun (next: HttpFunc) (ctx: HttpContext) ->
         task {
-            let products = ctx.GetService<GiraffeContext>().products.ToList<Product>() |> List.ofSeq
+            // REFACT!!!!!
+            let products =
+                ctx.GetService<GiraffeContext>().products.ToList<Product>() |> List.ofSeq
+
             let product =
                 match products with
                 | head :: _ when head.id = id -> Some(head)
@@ -24,16 +30,21 @@ let getById id : HttpHandler =
             return! json product next ctx
         }
 
-let add : HttpHandler =
-    fun (next: HttpFunc) (ctx: HttpContext) -> task {
-        let! product = ctx.BindJsonAsync<Product>()
-        return! json None next ctx
-    }
+let add: HttpHandler =
+    fun (next: HttpFunc) (ctx: HttpContext) ->
+        task {
+            // REFACT!!!!!
+            let! product = ctx.BindJsonAsync<Product>()
+            return! json None next ctx
+        }
 
 let remove id : HttpHandler =
     fun (next: HttpFunc) (ctx: HttpContext) ->
         task {
-            let products = ctx.GetService<GiraffeContext>().products.ToList<Product>() |> List.ofSeq
+            // REFACT
+            let products =
+                ctx.GetService<GiraffeContext>().products.ToList<Product>() |> List.ofSeq
+
             let rec remove (array: List<Product>, aux) =
                 match array with
                 | head :: tail when head.id = id -> tail @ aux
